326c9f4b16f4b3ec5db44e2cba2bda06bc90893a
Marthe Dybdahl <mad@profitbase.no>
2021-01-24 16:25:57 +0100

    Starting on informationboard

--- src/allvis/clientapp/src/App.tsx
+++ src/allvis/clientapp/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { AdminShell } from './app/admin/AdminShell';
+import { Informationboard } from './app/slideShow/Informationboard';
 import { SlideShowShell } from './app/slideShow/SlideShowShell';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "weather-icons/css/weather-icons.css"
 
 function App() {
   return (
     <div className="App">
-      <AdminShell />
-      <SlideShowShell />
+      <Informationboard />
     </div>
   );
 }
