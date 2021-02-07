17cb91902e62cd06528cce6b99cda484736e34e4
Marthe Dybdahl <mad@profitbase.no>
2021-02-07 21:08:47 +0100

    Fix det code at date.tsx

--- src/allvis/clientapp/src/app/slideShow/Informationboard/Date.tsx
+++ src/allvis/clientapp/src/app/slideShow/Informationboard/Date.tsx
@@ -7,45 +7,34 @@ import { FaRegCalendarAlt } from 'react-icons/fa';
 
 export const DateComponent = () => {
 
-    const [day, setDay] = useState<string>()
     const [date, setDate] = useState<string>()
-    const [month, setMonth] = useState<string>()
-    const [year, setYear] = useState<string>()
-    const [hour, setHour] = useState<string>()
-    const [minute, setMinute] = useState<string>()
+    const [time, setTime] = useState<string>()
     
     useEffect (() => {
         setInterval(() => {
-            dateBuilder(new Date())
+            dateBuilder()
         }, 1000);
     }, [])
 
     
       
-    const dateBuilder = (d: Date) => {
-        let day = d.toLocaleString("default", { weekday: "long" })
-        let date = d.toLocaleDateString("default", {day: 'numeric'})
-        let month = d.toLocaleString("default", { month: 'long' })
-        let year = d.toLocaleString("default", { year: 'numeric'});
-        let hour = d.toLocaleTimeString("default", {hour: 'numeric'})
-        let minutes = d.toLocaleTimeString("default", {minute: 'numeric'})
+    const dateBuilder = () => {
+        const event = new Date()
+        const date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const time = {hour: 'numeric', minute: 'numeric'}
 
-        setDay(day)
-        setDate(date)
-        setMonth(month)
-        setYear(year)
-        setHour(hour)
-        setMinute(minutes)
+        setDate(event.toLocaleDateString(undefined, date))
+        setTime(event.toLocaleTimeString(undefined, time))
 
     }
 
     return(
     <div>
         <div className={cx (styles.dateinfo)}>
-            <p> <FaRegCalendarAlt /> {day} {date}{month}.{year}</p>
+            <p> <FaRegCalendarAlt /> {date}</p>
         </div>
         <div className={cx (styles.clock)}>
-            <p>{hour}:{minute}</p>
+            <p>{time}</p>
         </div>
     </div>
     )
