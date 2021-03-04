b3b51e3b9836694fa2c097e4bd4e771248d7c869
marthedybdahl <54355967+marthedybdahl@users.noreply.github.com>
2021-03-04 08:31:28 +0100

    Update AdminShell.tsx

--- src/allvis/clientapp/src/app/admin/AdminShell.tsx
+++ src/allvis/clientapp/src/app/admin/AdminShell.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
 import cx from 'classnames'
 import {Twitter} from '../slideShow/TwitterAPI/Twitter'
-<<<<<<< HEAD
-=======
 import {Linkdin}  from './Linkdin'
->>>>>>> 1836a3e3b03c8736ff6104352e7c576cadc61f2f
+
 
 export const AdminShell = () => {
 
