8051727bd3118c2a19da05d6beae2d2a0711caf4
hanneforsberg <55483022+hanneforsberg@users.noreply.github.com>
2021-03-23 14:20:46 +0100

    admin side

--- src/allvis/clientapp/src/App.tsx
+++ src/allvis/clientapp/src/App.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.scss';
-import { AdminShell } from './app/admin/AdminShell';
 import defaultSlides from "./app/slideShow/sites-slide"
 import {SlideShowShell} from "./app/slideShow/SlideShowShell"
 import { Informationboard } from './app/slideShow/Informationboard/Informationboard';
+import { Button } from 'react-bootstrap';
+import Navigation from './app/admin/navbar';
+import Routes from "./app/admin/Routes"
+import AdminShell from "./app/admin/AdminShell";
 
 function App() {
   return (
     <div className="App">
        
-    <main>   
+    <main>  
+      <AdminShell/>
 
+      {/* <Navigation />
+      <Routes />
       <Informationboard/>
+      <SlideShowShell slides = {defaultSlides}/> */}
 
-      <SlideShowShell slides = {defaultSlides}/>
     </main>
-    
     </div>
   );
 }
--- src/allvis/clientapp/src/app/admin/AdminShell.tsx
+++ src/allvis/clientapp/src/app/admin/AdminShell.tsx
@@ -1,24 +1,125 @@
-import * as React from 'react'
+
 import cx from 'classnames'
-import {Twitter} from '../slideShow/TwitterAPI/Twitter'
-import {Linkdin}  from './Linkdin'
+import styles from "./AdminShell.module.scss"
+import logo from './logo.svg'
 
 
-export const AdminShell = () => {
+import React, { Component } from 'react';
+import { Card, FormControl, FormLabel, ListGroup } from 'react-bootstrap';
 
-     React.useEffect(() => {
-        const loaddata = async() => {
-            const result = await fetch('api/Twitter')
-            console.log(await result.json())
+
+
+class AdminShell extends Component<{},any>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+          showHideInput: true,
+          showHideLName: true
+        };
+        this.hideComponent = this.hideComponent.bind(this);
+      }
+    
+      hideComponent(name: string) {
+        switch (name) {
+          case "showHideInput":
+            this.setState({ showHideInput: !this.state.showHideInput });
+            break;
+          case "showHideInput2":
+            this.setState({ showHideInput2: !this.state.showHideInput2 });
+            break;
+          
         }
-        loaddata()
-     }, [])
+      }
+    render() {
+        const { showHideInput, showHideInput2 } = this.state;
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
+                <div className={cx(styles.informationboard)}>
+                <img className={cx(styles.logo)} src={logo} alt="Logo" />
+                    <h1 className={cx(styles.title)}>ALLVIS ADMIN SIDE</h1>
+                
+                
+                </div>
+                <div className={cx(styles.text)}>
+                    <h2>Administrer lysbildefremvisningen</h2>
+                    <div className="md-radio">
+                    <input id="1" type="radio" name="g" checked></input>
+                    <label htmlFor="1">Dato</label>
+                    </div>
+                    <div className="md-radio">
+                    <input id="2" type="radio" name="g"></input>
+                    <label htmlFor="2">Klokkw</label>
+                    </div>
+            
 
-    return(
+                    <div>
+                    <table>
+                    {showHideInput && (
+                        <tr>
+                        <td>Været :</td>
+                        <td>
+                            <input type="text" id="Input" />
+                        </td>
+                        </tr>
+                    )}
+                    {showHideInput2 && (
+                        <tr>
+                            <td>Lunsj :</td>
+                        <td>Mandag :</td>
+                        <td>
+                            <input type="text" id="Input2" />
+                        </td>
+                        <td>Tirsdag :</td>
+                        <td>
+                            <input type="text" id="Input2" />
+                        </td>
+                        <td>Onsdag :</td>
+                        <td>
+                            <input type="text" id="Input2" />
+                        </td>
+                        <td>Torsdag :</td>
+                        <td>
+                            <input type="text" id="Input2" />
+                        </td>
+                        <td>Fredag :</td>
+                        <td>
+                            <input type="text" id="Input2" />
+                        </td>
+                        </tr>
+                    )}
+                    {showHideInput && showHideInput2 && (
+                        <tr>
+                        <td>
+                            <button>Submit</button>
+                        </td>
+                        </tr>
+                    )}
+                    <tr>
+                        <td>
+                        <button onClick={() => this.hideComponent("showHideInput")}>
+                            Været
+                        </button>
+                        </td>
+                        <td>
+                        <button onClick={() => this.hideComponent("showHideInput2")}>
+                            Lunsj
+                        </button>
+                        </td>
+                    </tr>
+                    </table>
+                </div>
+                
+                
+                
+                
+            
+                </div> 
+                </div>
+            
 
-        <div>
-            <Linkdin/>
 
-        </div>
-    )
-}
+        );
+       }
+    }
+
+export default AdminShell;
