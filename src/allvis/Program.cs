7ddcedb6b00951fb2b0dfa6896bca4d427fb3659
marthe dybdahl <Marthe.dybdahl@outlook.com>
2021-04-25 12:22:02 +0200

    fixed half communication with database

--- src/allvis/Program.cs
+++ src/allvis/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace allvis
 {
@@ -14,6 +11,7 @@ namespace allvis
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
