975e4bd5cafa0ea1ab0c253035df4e26ffc32354
marthedybdahl <54355967+marthedybdahl@users.noreply.github.com>
2021-04-27 14:17:58 +0200

    Update App.tsx

--- src/allvis/clientapp/src/App.tsx
+++ src/allvis/clientapp/src/App.tsx
@@ -3,29 +3,8 @@ import './App.scss';
 import defaultSlides from "./app/slideShow/sites-slide"
 import {SlideShowShell} from "./app/slideShow/SlideShowShell"
 import { Informationboard } from './app/slideShow/Informationboard/Informationboard';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-//import { Button } from 'react-bootstrap';
-//import Navigation from './app/admin/navbar';
-//import Routes from "./app/admin/Routes"
-<<<<<<< HEAD
-=======
-//import { Button } from 'react-bootstrap';
-//import Navigation from './app/admin/navbar';
-//import Routes from "./app/admin/Routes"
-<<<<<<< HEAD
->>>>>>> Lagt til bruk av Facebook API
-import AdminShell from "./app/admin/AdminShell";
->>>>>>> Lagt til bruk av Facebook API
-=======
-=======
->>>>>>> update admin
 import {AdminShell} from "./app/admin/AdminShell";
 
-
->>>>>>> update admin
 
 function App() {
   return (
